84e70591962c4b763fa2a2c98a0ef2a9d690c0be
Safnas <iamsafnas@gmail.com>
2020-12-27 23:57:41 +0530

    search box working

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -13,6 +13,7 @@ export class ProductListComponent implements OnInit {
   products: Product[] = [];
   currentCategoryId: number = 0;
   currentCategoryName: string = '';
+  searchMode: boolean = false;
 
   constructor(private productService : ProductService,
               private route: ActivatedRoute) { }
@@ -25,6 +26,24 @@ export class ProductListComponent implements OnInit {
 
   listProducts(){
 
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+
+    if(this.searchMode){
+      this.handleSearchProducts();
+    }else{
+      this.handleListProducts();
+    }
+  }
+
+  handleSearchProducts(){
+    const theKeyword = this.route.snapshot.paramMap.get('keyword');
+
+    this.productService.searchProducts(theKeyword).subscribe(data =>{
+      this.products = data;
+    })
+  }
+
+  handleListProducts(){
     //check if "id" paramter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
