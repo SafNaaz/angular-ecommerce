078b09828abc79d24ecd48050554c70347f76efc
Safnas <iamsafnas@gmail.com>
2020-12-27 18:26:57 +0530

    added table styles and removed some warnings

--- src/app/common/product.ts
+++ src/app/common/product.ts
@@ -1,11 +1,11 @@
 export class Product {
-  sku: string;
-  name: string;
-  description: string;
-  unitPrice: number;
-  imangUrl: string;
-  active: boolean;
-  unitsInStock: number;
-  dataCreated: Date;
-  lastUpdated: Data;
+  sku!: string;
+  name!: string;
+  description!: string;
+  unitPrice!: number;
+  imangUrl!: string;
+  active!: boolean;
+  unitsInStock!: number;
+  dataCreated!: Date;
+  lastUpdated!: Date;
 }
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -4,12 +4,13 @@ import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
+  //templateUrl: './product-list.component.html',
+  templateUrl: './product-list-table.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
-  products : Product[];
+  products: Product[] = [];
 
   constructor(private productService : ProductService) { }
 
